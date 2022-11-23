0dfba470b770ba4f7599fdd00e1efad965385971
Nipun Batra <nipunbatra0@gmail.com>
2022-11-23 14:29:25 +0530

    added

---
+++ Introduction/euler1.py
@@ -0,0 +1,14 @@
+# Method 1
+
+s = 0
+for i in range(1000):
+    if i % 3 == 0 or i % 5 == 0:
+        s = s + i
+
+print(s)
+
+
+# Method 2
+s2 = sum(range(3, 1000, 3)) + sum(range(5, 1000, 5)) - sum(range(15, 1000, 15))
+
+print(s2)
